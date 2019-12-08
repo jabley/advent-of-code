dc4c6a9ca1a784978e435fc2f33a23e4cf55de36
James Abley <james.abley@gmail.com>
2019-12-08 22:29:47 +0000

    Solve 2019 Day 08, part 2
    See https://adventofcode.com/2019/day/8#part2

--- 2019/day08/main.rb
+++ 2019/day08/main.rb
@@ -7,3 +7,22 @@ layers = input.each_slice(WIDTH*HEIGHT)
 
 layers.min_by {|layer| layer.count(0) }
   .tap { |layer| puts "Part1: #{ layer.count(1) * layer.count(2) }" }
+
+BLACK = 0
+WHITE = 1
+TRANSPARENT = 2
+
+def stack_pixels(a, b)
+  a == TRANSPARENT ? b : a
+end
+
+image = layers
+  .to_a
+  .transpose
+  .map { |pixel| pixel.reduce(&method(:stack_pixels)) }
+
+puts "Part2:"
+puts image
+  .each_slice(WIDTH)
+  .map { |row| row.map(&{ BLACK => ' ', WHITE => '*' }) }
+  .map(&:join)
