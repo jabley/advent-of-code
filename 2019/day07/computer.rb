fc9f82428e143416297d6118811ca166a438848e
James Abley <james.abley@gmail.com>
2019-12-09 19:51:36 +0000

    Adjust 2019 day 07 computer in-line with day 09 puzzle

--- 2019/day07/computer.rb
+++ 2019/day07/computer.rb
@@ -1,26 +1,33 @@
 class Computer
-  Parameter = Struct.new(:mode, :value, :index) do
+  Parameter = Struct.new(:mode, :value, :write, :index) do
 
+    RELATIVE_MODE = 2
     IMMEDIATE_MODE = 1
     POSITION_MODE  = 0
 
     MODES = {
       IMMEDIATE_MODE => "Immediate",
       POSITION_MODE  => "Position",
+      RELATIVE_MODE  => "Relative",
     }
 
-    def self.parse(instruction, ip, position, intcodes)
+    def self.parse(instruction, ip, relative_base, position, memory)
       mode  = (instruction / (10**(position+1))) % 10
-      index = intcodes[ip+position]
+      index = memory[ip+position]
 
-      value = case mode
+      case mode
+      when RELATIVE_MODE
+        value  = memory[relative_base+index]
+        write = ->(val) { memory[relative_base+index] = val }
       when IMMEDIATE_MODE
-        index
+        value  = index
+        write = ->(val) { memory[index] = val }
       when POSITION_MODE
-        intcodes[index]
+        value  = memory[index]
+        write = ->(val) {  memory[index] = val }
       end
 
-      Parameter.new(mode, value, index)
+      Parameter.new(mode, value, write, index)
     end
 
     def to_s
@@ -35,7 +42,10 @@ class Computer
   attr_reader :output
 
   def initialize(program, input: [], output: [], debug: false)
-    @p      = program.dup
+    @memory = program.reduce(Hash.new { |h, k| h[k] = 0}) do |acc, val|
+      acc[acc.size] = val
+      acc
+    end
     @input  = input
     @output = output
     @debug  = debug
@@ -44,9 +54,10 @@ class Computer
   def eval
     # start by looking at the first integer (called position 0)
     ip = 0
+    relative_base = 0
 
     loop do
-      instruction = @p[ip]
+      instruction = @memory[ip]
       opcode      = instruction % 100
 
       puts "evaluating instruction #{ instruction } with opcode #{ opcode } at #{ ip }" if @debug
@@ -54,26 +65,28 @@ class Computer
       break if opcode == 99
 
       # All other opcodes take at least a single parameter.
-      p1 = Parameter.parse(instruction, ip, 1, @p)
+      p1 = Parameter.parse(instruction, ip, relative_base, 1, @memory)
 
       # We can safely parse as a parameter the opcode which follows opcodes that only have a
       # single parameter (read and print)
-      p2 = Parameter.parse(instruction, ip, 2, @p)
+      p2 = Parameter.parse(instruction, ip, relative_base, 2, @memory)
+
+      p3 = Parameter.parse(instruction, ip, relative_base, 3, @memory)
 
       case opcode
       when 1 # add
-        puts "Add #{ p1 } + #{ p2 } into (Position #{ @p[ip+3] })" if @debug
-        @p[@p[ip+3]] = p1.value + p2.value
+        puts "Add #{ p1 } + #{ p2 } into (Position #{ @memory[ip+3] })" if @debug
+        p3.write.call(p1.value + p2.value)
         ip += 4
       when 2 # mult
-        puts "Mult #{ p1 } * #{ p2 } into (Position #{ @p[ip+3] })" if @debug
-        @p[@p[ip+3]] = p1.value * p2.value
+        puts "Mult #{ p1 } * #{ p2 } into (Position #{ @memory[ip+3] })" if @debug
+        p3.write.call(p1.value * p2.value)
         ip +=4
       when 3 # read
         val = @input.shift
         puts "SetInputAt #{ val } into (Position #{ p1.index })" if @debug
         ip += 2
-        @p[p1.index] = val
+        p1.write.call(val)
       when 4 # print
         puts "Print #{ p1 } : #{ p1.value }" if @debug
         @output.push(p1.value)
@@ -86,12 +99,16 @@ class Computer
         ip = p1.value.zero? ? p2.value : ip+3
       when 7 # less than
         puts "LessThan #{ p1 } #{ p2 }" if @debug
-        @p[@p[ip+3]] = p1.value < p2.value ? 1 : 0
+        p3.write.call( (p1.value < p2.value) ? 1 : 0 )
         ip += 4
       when 8 # equals
         puts "Equals #{ p1 } #{ p2 }" if @debug
-        @p[@p[ip+3]] = p1.value == p2.value ? 1 : 0
+        p3.write.call( (p1.value == p2.value) ? 1 : 0 )
         ip += 4
+      when 9 #adjust-relative-base
+        puts "AdjustRelativeBase #{ p1 }"
+        relative_base += p1.value
+        ip += 2
       else raise "Invalid opcode #{ opcode }"
       end
     end
