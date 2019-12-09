fcaa48c1d3f629231fff2d5718fcdcb241579c64
James Abley <james.abley@gmail.com>
2019-12-09 20:00:03 +0000

    Solve 2019 Day 09 part 2
    This was essentially for free, now that we have a fully working IntCode
    computer.

--- 2019/day09/main.rb
+++ 2019/day09/main.rb
@@ -6,3 +6,8 @@ c = Computer.new(input, input: [1], debug: false)
 c.eval
 
 puts "Part1: #{ c.output.last }"
+
+c = Computer.new(input, input: [2], debug: false)
+c.eval
+
+puts "Part2: #{ c.output.last }"
